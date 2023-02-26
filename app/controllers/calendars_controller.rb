08666a169ff8b1428f0d7cebc35d3d35023590cc
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-02-26 22:56:40 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -25,8 +25,10 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    # wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    # https://docs.ruby-lang.org/ja/2.6.0/class/Date.html
 
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -40,7 +42,41 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      # days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdaysから値を取り出す記述}
+
+      
+      wday_num = Date.today.wday + x
+      # Memo1
+      # wdays[]
+      # Memo2
+      # @todays_date + x
+      # wday_num = Date.today.wday + x　今日の曜日のwdayを呼び出す。timesメソッドのxと連結
+
+        # if wday_num >= 7  //7で繰り返す
+        # wday_num = wday_num -7
+        # end
+      # [0,1,2,3,4,5,6]
+      # wdayメソッドを用いて取得した数値
+      if #「wday_numが7以上の場合」という条件式
+        wday_num >= 7
+        wday_num = wday_num -7
+
+
+      end
+
+      days = { month: (@todays_date + x).month, date: @todays_date.day + x,
+        # :month => (@todays_date + x).month, :date => @todays_date + x,
+        #  (@todays_date + x).day, 
+        
+        # :plans => today_plans, 
+        # :wday => [(@todays_date + x).wday], plans :plans
+        wday: wdays[(@todays_date + x).wday], plans: plans
+        # wdaysから値を取り出す記述
+      }
+
+      
       @week_days.push(days)
     end
 
